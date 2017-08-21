da5d7d24738dee99dbf5d8ec1678c8805d09a083
Thanya Moolmuang <tmoolmuang@yahoo.com>
2017-08-20 20:34:11 -0700

    add sendgrid heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,6 +83,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  config.action_mailer.default_url_options = { host: 'tm-student.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'student-tm.herokuapp.com' }
 end
